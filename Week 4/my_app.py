2431f42e40a5a0d62ae1eabb81767bc46e7707dd
Tommy Santarelli <tsantare@nd.edu>
2026-02-03 10:33:41 -0500

    First Streamlit App

---
+++ Week 4/my_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# Similar to singular markdown hashtag - creates a large header text
+st.title("Hello, Streamlit!")
+st.markdown("# Hello, Streamlit!")
+
+st.write("This is my first Streamlit app.")
+
+if st.button("Click Me!"):
+    st.write("You clicked the button!")
+else:
+    st.write("Click the button and see what happens!.")
+
+### Loading our CSV file and displaying it in a table
+
+import pandas as pd
+
+st.subheader("Exploring our Dataset")
+
+# Load in the CSV file
+df = pd.read_csv("data/sample_data-1.csv")
+
+st.write("Here's our data!")
+st.dataframe(df)
+
+city = st.selectbox("Select a city", df["City"].unique(), index=None)
+filtered_df = df[df["City"] == city]
+
+st.write(f"People in {city}")
+st.dataframe(filtered_df)
+
+## Add bar chart
+st.bar_chart(df["Salary"])
